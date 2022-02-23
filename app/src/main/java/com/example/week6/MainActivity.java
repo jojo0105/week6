56b608f681a6286e46b82323f7d7292270ff4ad2
jojo0105 <georgeaziz88@yahoo.com>
2022-02-23 15:21:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/week6/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.week6;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText name, contact, dob;
+    Button insert, update, delete, view;
+    DBHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        contact = findViewById(R.id.contact);
+        dob = findViewById(R.id.dob);
+
+        insert = findViewById(R.id.btnInsert);
+        update = findViewById(R.id.btnUpdate);
+        delete = findViewById(R.id.btnDelete);
+        view = findViewById(R.id.btnView);
+
+        DB = new DBHelper(this);
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameTXT = name.getText().toString();
+                String contactTXT = contact.getText().toString();
+                String dobTXT = dob.getText().toString();
+
+                Boolean checkInsertData = DB.insertuserdata(nameTXT, contactTXT, dobTXT);
+
+                if(checkInsertData)
+                    Toast.makeText(MainActivity.this, "New Entry Inserted", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(MainActivity.this, "New Entry Not Inserted", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameTXT = name.getText().toString();
+                String contactTXT = contact.getText().toString();
+                String dobTXT = dob.getText().toString();
+
+                Boolean checkUpdateData = DB.insertuserdata(nameTXT, contactTXT, dobTXT);
+
+                if(checkUpdateData)
+                    Toast.makeText(MainActivity.this, "Entry Updated", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(MainActivity.this, "Entry Not Updated", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameTXT = name.getText().toString();
+                String contactTXT = contact.getText().toString();
+                String dobTXT = dob.getText().toString();
+
+                Boolean checkDeleteData = DB.insertuserdata(nameTXT, contactTXT, dobTXT);
+
+                if(checkDeleteData)
+                    Toast.makeText(MainActivity.this, "Entry Deleted", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(MainActivity.this, "Entry Not Deleted", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = DB.getdata();
+
+                if (res.getCount() == 0){
+                    Toast.makeText(MainActivity.this, "No Entry Exists", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                StringBuffer stringBuffer = new StringBuffer();
+
+                while(res.moveToNext()){
+                    stringBuffer.append("Name: " + res.getString(0) + "\n");
+                    stringBuffer.append("Contact: " + res.getString(1) + "\n");
+                    stringBuffer.append("Date of Birth: " + res.getString(2) + "\n");
+                }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setCancelable(true);
+                builder.setTitle("User Entries");
+                builder.setMessage(stringBuffer.toString());
+                builder.show();
+            }
+        });
+    }
+}
